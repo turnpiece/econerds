58e1b93d818fb3f42c590749afbfb0930c439a4b
Paul Jenkins <paul@turnpiece.com>
2017-10-03 20:48:20 +0100

    update plugins & themes

--- wp-content/plugins/snapshot/views/full/_backups-list.php
+++
@@ -1,3 +0,0 @@
-<div class="backup-list">
-	<?php Snapshot_View_Template::get('form')->load('backups_list', array('model' => $model)); ?>
-</div>
--- wp-content/plugins/snapshot/views/full/destination.php
+++
@@ -1,9 +0,0 @@
-<div class="snapshot-destination full">
-	<?php
-		$template = $model->has_dashboard() && $model->is_active()
-			? 'destination-backups'
-			: 'destination-config'
-		;
-		$this->load($template, array('model' => $model));
-	?>
-</div>
